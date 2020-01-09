874998525e784fc2adcaa0c34ab34a91c9ec9b14
nikanurov1 <43588601+nikanurov1@users.noreply.github.com>
2020-01-09 15:01:15 +0300

    Homework

---
+++ RIP-dz/main/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from . import models
+
+admin.site.site_header = "Мой список дел"
+
+
+@admin.register(models.Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    search_fields = ('user__username',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(models.Events)
+class EventsAdmin(admin.ModelAdmin):
+    search_fields = ('event__title',)
---
+++ RIP-dz/main/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Tasks, Events
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Tasks
+        fields = ('title', 'description', 'created')
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Events
+        fields = ('title', 'description', 'term', 'event')
